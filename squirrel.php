84b983c65ff1ce9752d55e55f374e555f1adf13b
Eldar Marcussen <emarcussen@seek.com.au>
2021-11-18 16:36:12 +1100

    Practice safe coding

--- squirrel.php
+++ squirrel.php
@@ -1,10 +1,12 @@
 <?php
 // RageEx regular expression CTF - By Eldar "Wireghoul" Marcussen 
+//Validate input
 $safe = preg_replace('/[^A-z,*]/','', $_GET['columns']);
-$query = "select `$safe` from user";
-echo "$query\n";
+echo "$safe\n";
 
 $db = mysqli_connect("localhost","root","toor","mydb");
+// Escape output
+$query = "select `".mysqli_real_escape_string($db, $safe)." from user";
 $res = mysqli_query($db, $query);
 $data = mysqli_fetch_array($res);
 print_r($data);
